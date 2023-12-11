37e2797d8af979c2abecd5a4734b3c47536ef9ec
KeerthanaMurugan01 <146663459+KeerthanaMurugan01@users.noreply.github.com>
2023-12-11 22:32:15 +0530

    Delete password directory

--- password/login.js
+++
@@ -1,8 +0,0 @@
-// login.js
-function login() {
-    // Implement login logic here
-    console.log("Login logic goes here");
-    // For this example, redirect to a welcome page after successful login
-    window.location.href = "welcome.html";
-  }
-  
